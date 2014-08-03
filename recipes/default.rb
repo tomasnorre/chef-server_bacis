e8f9d678c8444bad0afe319edb40df5aae1605b0
Tomas Mikkelsen <tomas.mikkelsen@aoe.com>
2014-08-03 22:53:00 +0200

    Added vim and htop

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,21 @@
 # Cookbook Name:: server_basic
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, Tomas Norre Mikkelsen
 #
 # All rights reserved - Do Not Redistribute
 #
+
+packages=[
+	'htop',
+  'vim'
+]
+
+case node[:platform]
+when 'debian','ubuntu'
+	packages.each do |pkg|
+		package pkg do
+			action :upgrade
+		end
+	end
+end
