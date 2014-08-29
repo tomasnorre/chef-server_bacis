8e588423ad6d6ecdcef6740025659f44bb223f74
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2014-08-30 01:03:04 +0200

    [TASK] Added packages & packagespurge

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,4 @@ maintainer_email 'tomasnorre@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures server_basic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-
-depends 'git'
+version '0.2.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,17 +7,37 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'git'
+packagespurge = %w(
+   apparmor
+   apparmor-utils
+   libapparmor-perl
+   libapparmor1
+   ubuntu-minimal
+   command-not-found
+   command-not-found-data
+   landscape-common
+)
 
 packages = %w(
   'htop'
   'sed'
   'vim'
   'wget'
+  'git'
 )
 
-packages.each do |pkg|
-  package pkg do
-    action :upgrade
-  end
+
+
+case node['platform']
+when 'debian', 'ubuntu'
+   packagespurge.each do |pkg|
+     package pkg do
+       action :remove
+     end
+   end
+   packages.each do |pkg|
+     package pkg do
+       action :upgrade
+     end
+   end
 end
