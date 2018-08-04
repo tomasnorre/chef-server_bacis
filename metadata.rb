3cb812c446d04a0b4394ec50b3612ef87bf8080a
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2018-08-04 11:01:24 +0200

    [TASK] Switched to MIT license

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name 'server_basic'
 maintainer 'Tomas Norre Mikkelsen'
 maintainer_email 'tomasnorre@gmail.com'
-license 'Unlicense'
+license 'MIT'
 description 'Installs/Configures server_basic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/tomasnorre/chef-server_bacis'
 issues_url 'https://github.com/tomasnorre/chef-server_bacis/issues'
 chef_version '~> 12'
 supports 'ubuntu'
-version '0.3.2'git 
+version '0.3.2'
